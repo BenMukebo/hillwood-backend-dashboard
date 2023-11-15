95595026d8e5ba3c2aa079fda8a0e92baf48fddb
BenMukebo <l.benkasmukebo7@gmail.com>
2023-11-15 12:50:02 +0200

    Add cors.rb file and update the routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,14 +13,19 @@ class User < ActiveRecord::Base
   after_initialize :set_default_role, if: :new_record?
 
   # add sign up validation
-  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 24 }
+  validates :username, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 24 }, on: :update, unless: :admin? #, on: :create, unless: :admin?
   validates :email, presence: true
-  validates :phone, presence: true, uniqueness: true, length: { within: 10..14 }
+  # validates :avatar_url, presence: true, uniqueness: true, length: { within: 10..14 }
   validates :password_confirmation, presence: true, allow_blank: true
   validates :terms_of_service, acceptance: { accept: true }
   # validates :privacy_policy, presence: true, inclusion: { in: [true, false] }
   validates_inclusion_of :remember_me, in: [true, false], allow_blank: true
   validates_inclusion_of :welcome_email_send, in: [true, false]
+  # validates_inclusion_of :email_confirmed, in: [true, false]
+
+  AGE_GROUP = { infants: 0, children: 1, adolescents: 2, adults: 3, older: 4 }.freeze
+  enum age_group: AGE_GROUP, _prefix: true
+  # validates :age_group, :inclusion => {:in => AGE_GROUP}
 
   def admin?
     role.name == 'admin'
