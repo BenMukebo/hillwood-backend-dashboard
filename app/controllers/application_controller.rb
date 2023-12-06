b2336a5b068d6067a961aba326118ffe1c0b1650
BenMukebo <l.benkasmukebo7@gmail.com>
2023-12-07 01:39:08 +0200

    Call all the rescue_from ActiveRecord error and intercept them from the application controller to the ResponseHelper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,17 @@ class ApplicationController < ActionController::Base
   # protect_from_forgery with: :null_session
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-  rescue_from ActionDispatch::Http::Parameters::ParseError, with: :handle_json_parse_errors
+  rescue_from ArgumentError, with: :argument_error
+  # rescue_from ActionDispatch::Http::Parameters::ParseError, with: :argument_error
+  # TODO fix ActionDispatch::Http::Parameters::ParseError (795: unexpected token at ''):
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  rescue_from ActiveRecord::RecordNotDestroyed, with: :record_not_destroyed
+  rescue_from ActiveRecord::RecordInvalid, with: :record_invalid
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
+  rescue_from ActiveRecord::RecordNotSaved, with: :record_not_saved
+  rescue_from ActiveRecord::RecordNotUnique, with: :record_not_unique
+  rescue_from ActionController::UnpermittedParameters, with: :unpermitted_parameters
+  rescue_from ActionController::ParameterMissing, with: :parameter_missing
 
   private
 
@@ -24,9 +34,37 @@ class ApplicationController < ActionController::Base
     redirect_back(fallback_location: root_path)
   end
 
-  def handle_json_parse_errors
-    # binding.pry
-    render_unprocessable_entity_response("Invalid JSON: #{request.body.read}")
+  def argument_error(error)
+    render_argument_error_response("Argument Error: #{error.message}")
+    # render_unprocessable_entity_response('Argument Error: ' + error.message)
+  end
+
+  def record_not_found(error)
+    render_not_found_response(error.message)
+  end
+
+  def record_not_destroyed(error)
+    render_unprocessable_entity_response(error.message)
+  end
+
+  def record_invalid(error)
+    render_unprocessable_entity_response(error.message)
+  end
+
+  def record_not_unique(error)
+    render_unprocessable_entity_response(error.message)
+  end
+
+  def record_not_saved(error)
+    render_unprocessable_entity_response(error.message)
+  end
+
+  def unpermitted_parameters(error)
+    render_unprocessable_entity_response(error.message)
+  end
+
+  def parameter_missing(error)
+    render_unprocessable_entity_response(error.message)
   end
 
   protected
--- app/controllers/concerns/response_helper.rb
+++ app/controllers/concerns/response_helper.rb
@@ -3,14 +3,19 @@ module ResponseHelper
   def render_success_response(message, data)
     render json: {
       success: true, # status: 'SUCCESS'
+      successCode: 200,
       message: message,
-      data: data
+      data: data,
+      currentPage: 1,
+      # perPage: 10,
+      totalPage: 1
     }, status: :ok
   end
 
   def render_not_found_response(message)
     render json: {
       success: false,
+      statusCode: 404,
       message: message,
       data: nil
     }, status: :not_found
@@ -25,10 +30,10 @@ module ResponseHelper
     }, status: error.status
   end
 
-  # unprocessable_entity
   def render_unprocessable_entity_response(error = 'Unprocessable entity')
     render json: {
       success: false,
+      statusCode: 422,
       errors: error
     }, status: :unprocessable_entity
   end
@@ -36,15 +41,43 @@ module ResponseHelper
   def render_unauthorized_response(message = 'Unauthorized')
     render json: {
       success: false,
+      statusCode: 401,
       message: message,
       data: nil
     }, status: :unauthorized
+  end
+
+  def render_argument_error_response(error = 'Argument error')
+    render json: {
+      success: false,
+      statusCode: 400,
+      errors: error
+    }, status: :bad_request
+  end
+
+  def render_no_content_response(message)
+    render json: {
+      success: true,
+      successCode: 204,
+      message: message,
+      data: nil
+    }, status: 201
+  end
+
+  def render_bad_request_response(message)
+    render json: {
+      success: false,
+      statusCode: 400,
+      message: message,
+      data: nil
+    }, status: :bad_request
   end
 
   def render_missing_token_response(message = 'Missing token')
     render json: {
       success: false,
       message: message,
+      statusCode: 401,
       data: nil
     }, status: :unauthorized
   end
@@ -58,3 +91,15 @@ module ResponseHelper
   end
 end
 # rubocop: enable Style/HashSyntax
+
+# rescue_from ActionDispatch::Http::Parameters::ParseError, with: :handle_json_parse_errors
+# before_action :handle_json_parse_errors, if: :json_request?
+
+# def json_request?
+#   request.format.json?
+# end
+
+# rescue_from ApiError do |exception|
+#   log_error(exception)
+#   render json: { error: exception.message }, status: exception.status
+# end
