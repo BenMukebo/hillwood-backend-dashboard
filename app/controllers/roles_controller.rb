5401f42aeb2a7d527f7a4300f60a24645161161e
BenMukebo <l.benkasmukebo7@gmail.com>
2023-12-07 06:42:33 +0200

    Update the schema and skip authentication on the /roules actions

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -1,7 +1,8 @@
 class RolesController < ApplicationController
   before_action :set_role, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user! #, only: %i[index show]
+  # skip_after_action :verify_authorized, only: [:index, :show]
 
-  # GET /roles or /roles.json
   def index
     @roles = Role.all
   end
