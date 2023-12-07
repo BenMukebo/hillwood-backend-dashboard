1376d1e57b0e26d4a55e59d99458fb3012764b3d
BenMukebo <l.benkasmukebo7@gmail.com>
2023-12-07 20:11:32 +0200

    Setting up Active Admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   mount_devise_token_auth_for 'User', at: 'auth'
 
   # root to: 'admin/dashboard#index'
-  # root "homes#index"
+  root "homes#index"
   
   resources :roles
   namespace :api, defaults: { format: :json } do
@@ -19,3 +22,12 @@ Rails.application.routes.draw do
 
   # get "up" => "rails/health#show", as: :rails_health_check
 end
+
+# https://devise-token-auth.gitbook.io/devise-token-auth/usage/multiple_models
+# https://github.com/lynndylanhurley/devise_token_auth/blob/master/docs/usage/multiple_models.md
+# https://stackoverflow.com/questions/73572579/devise-token-auth-customised-routes-for-additonal-endpoints
+
+# Email Template Overrides
+# https://devise-token-auth.gitbook.io/devise-token-auth/usage/overrides
+
+# Create an api  namespace and move the rote inside and create an user API
