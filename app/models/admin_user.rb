cce0d296b58a7a69dbe9eb2ebd00d72649eea9d7
BenMukebo <l.benkasmukebo7@gmail.com>
2023-12-15 23:12:20 +0200

    Add self.ransackable_attributes method to fix the issue as suggeste by active admin gem

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -3,4 +3,8 @@ class AdminUser < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :trackable,
          :recoverable, :rememberable, :validatable
+  
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "current_sign_in_at", "current_sign_in_ip", "email", "encrypted_password", "id", "last_sign_in_at", "last_sign_in_ip", "remember_created_at", "reset_password_sent_at", "reset_password_token", "sign_in_count", "updated_at"]
+  end
 end
