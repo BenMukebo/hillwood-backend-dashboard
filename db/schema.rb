214ff1b26a120296af6286b4b7f801afaebfab2b
BenMukebo <l.benkasmukebo7@gmail.com>
2023-12-27 05:11:46 +0200

    Create movie_genres modal, micration and add name validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_21_231637) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_26_115646) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,6 +43,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_21_231637) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "movie_genres", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_movie_genres_on_name", unique: true
   end
 
   create_table "roles", force: :cascade do |t|
