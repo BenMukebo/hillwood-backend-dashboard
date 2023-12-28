dabb97413b691a0ca43430a94d875050b2177bd7
BenMukebo <l.benkasmukebo7@gmail.com>
2023-12-28 13:48:45 +0200

    Create a movie_writter modal migration  and add his column validations

---
+++ db/migrate/20231228082152_create_movie_writters.rb
@@ -0,0 +1,15 @@
+class CreateMovieWritters < ActiveRecord::Migration[7.0]
+  def change
+    create_table :movie_writters do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :avatar_url
+      t.jsonb :personal_details, null: false, default: { address: nil, bio: nil, date_of_birth: nil, first_name: nil, last_name: nil, sex: nil, interests: [], languages: [] }
+      t.integer :status
+
+      t.timestamps
+    end
+
+    add_index :movie_writters, :personal_details, using: :gin
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_12_26_115646) do
+ActiveRecord::Schema[7.0].define(version: 2023_12_28_082152) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,17 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_26_115646) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["name"], name: "index_movie_genres_on_name", unique: true
+  end
+
+  create_table "movie_writters", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "avatar_url"
+    t.jsonb "personal_details", default: {"bio"=>nil, "sex"=>nil, "address"=>nil, "interests"=>[], "languages"=>[], "last_name"=>nil, "first_name"=>nil, "date_of_birth"=>nil}, null: false
+    t.integer "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["personal_details"], name: "index_movie_writters_on_personal_details", using: :gin
   end
 
   create_table "roles", force: :cascade do |t|
