743b7c459ced55f5ad4fe8e277f438671447fc3d
BenMukebo <l.benkasmukebo7@gmail.com>
2024-01-18 02:11:15 +0200

    Generate AA episode dashboard panel

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -13,4 +13,13 @@ class Episode < ApplicationRecord
 
   enum status: { unreleased: 0, released: 1, banned: 2 }, _default: 'unreleased', _prefix: true
   validates :status, presence: true, inclusion: { in: statuses.keys }
+
+  def self.ransackable_attributes(_auth_object = nil)
+    %w[created_at description duration id image_url name released_at season_id serie_id status
+       trailer_link_id updated_at video_link_id]
+  end
+
+  def self.ransackable_associations(_auth_object = nil)
+    %w[season serie trailer_link video_link]
+  end
 end
